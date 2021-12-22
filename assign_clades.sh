8c355b01523112749c1878fbcf2a9e573910103f
Lena Schimmel <mail@lenaschimmel.de>
2021-12-23 00:07:31 +0100

    Add missing line in script.

--- assign_clades.sh
+++ assign_clades.sh
@@ -8,3 +8,5 @@ nextclade --in-order --input-fasta data/filtered_by_TACCGGTAT.fasta --input-data
 rm data/nextclade-output/*.fasta
 xsv select -d ';' seqName,clade data/result_clades_and_stuff.csv > data/result_clades.csv
 xsv join seqName data/result_clades.csv IMS_ID data/latest.csv | xsv select 'IMS_ID,DATE_DRAW,RECEIVE_DATE,PROCESSING_DATE,SENDING_LAB_PC,SEQUENCING_LAB_PC,SEQ_REASON,clade' > data/result_clades_and_metadata.csv
+
+grep "(Omicron)" data/result_clades_and_metadata.csv >> data/result_omicron_metadata.csv
