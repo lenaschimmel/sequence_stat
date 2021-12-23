4ec2cc2452e56f159923d6e1451d1c1f2ca2915e
Lena Schimmel <mail@lenaschimmel.de>
2021-12-23 13:03:57 +0100

    Forgot a line which adds the headers to data/result_omicron_metadata.csv. Also this meant that each execution of assign_clades.sh would append the results, leading to duplicate cases.

--- assign_clades.sh
+++ assign_clades.sh
@@ -9,4 +9,5 @@ rm data/nextclade-output/*.fasta
 xsv select -d ';' seqName,clade data/result_clades_and_stuff.csv > data/result_clades.csv
 xsv join seqName data/result_clades.csv IMS_ID data/latest.csv | xsv select 'IMS_ID,DATE_DRAW,RECEIVE_DATE,PROCESSING_DATE,SENDING_LAB_PC,SEQUENCING_LAB_PC,SEQ_REASON,clade' > data/result_clades_and_metadata.csv
 
+head -n 1 data/result_clades_and_metadata.csv > data/result_omicron_metadata.csv
 grep "(Omicron)" data/result_clades_and_metadata.csv >> data/result_omicron_metadata.csv
